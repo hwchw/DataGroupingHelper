773639898d9d1a3e63f63b31715808fb6e25d922
Hwan Wen Chen <munga0923@gmail.com>
2017-06-15 00:29:17 +0800

    Create second test case

--- DatasGroupingHelper/DatasGroupingHelperTests/DataGroupingHelperTests.cs
+++ DatasGroupingHelper/DatasGroupingHelperTests/DataGroupingHelperTests.cs
@@ -18,5 +18,16 @@ namespace DatasGroupingHelperTests
 			//assert
 			expected.ToExpectedObject().ShouldEqual(actual);
 		}
+
+		[TestMethod()]
+		public void GetSpecificDataSumWithRowCounts_given_DataName_id_countPerGroup_4_expected_is_6_15_24_21()
+		{
+			var sumRowHelper = new DataGroupingHelper();
+
+			var expected = new[] { 10, 26, 30 };
+			var actual = sumRowHelper.GetSpecificDataSumWithRowCounts("id", 4);
+
+			expected.ToExpectedObject().ShouldEqual(actual);
+		}
 	}
 }
