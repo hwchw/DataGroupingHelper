4e70c28108fd18c19fbbe30c65dd127ba65e9499
Hwan Wen Chen <munga0923@gmail.com>
2017-06-14 23:54:56 +0800

    Add first test case and init datas

---
+++ DatasGroupingHelper/DatasGroupingHelper/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatasGroupingHelper
+{
+	public class DataGroupingHelper : IGroupingMethods
+	{
+		private readonly Dictionary<string, List<object>> _products;
+
+		public DataGroupingHelper()
+		{
+			_products = new Dictionary<string, List<object>>
+			{
+				{"id", new List<object> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}},
+				{"Cost", new List<object> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1}},
+				{"Revenue", new List<object> {"11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"}},
+				{"SellPrice", new List<object> {21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31}}
+			};
+		}
+
+		public int[] GetSpecificDataSumWithRowCounts(string dataName, int rowCountOfOneGroup)
+		{
+			return new int[0];
+		}
+	}
+
+	internal interface IGroupingMethods
+	{
+		int[] GetSpecificDataSumWithRowCounts(string dataName, int rowCountOfOneGroup);
+	}
+}
---
+++ DatasGroupingHelper/DatasGroupingHelperTests/DataGroupingHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using DatasGroupingHelper;
+using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatasGroupingHelperTests
+{
+	[TestClass()]
+	public class DataGroupingHelperTests
+	{
+		[TestMethod()]
+		public void GetSpecificDataSumWithRowCounts_given_DataName_id_countPerGroup_3_expected_is_6_15_24_21()
+		{
+			//arrange
+			var sumRowHelper = new DataGroupingHelper();
+			//act
+			var expected = new[] { 6, 15, 24, 21 };
+			var actual = sumRowHelper.GetSpecificDataSumWithRowCounts("id", 3);
+			//assert
+			expected.ToExpectedObject().ShouldEqual(actual);
+		}
+	}
+}
